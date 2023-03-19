f10702d863dcfaf66f206a80be779c60dd7fda86
devneto <miguel.neto320@gmail.com>
2023-03-19 18:42:25 -0300

    feat: variables

---
+++ go/basics/variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var name string = "Un4uthorized"
+	fmt.Println(name)
+
+	// Declare a variable of type int set to its zero value.
+	var number int
+	fmt.Println(number)
+
+	//The := syntax is shorthand for declaring and initializing
+	fruit := "apple"
+	fmt.Println(fruit)
+
+}
